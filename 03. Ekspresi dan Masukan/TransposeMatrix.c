f5f4b8d8940b0af8ac599c824be7eaf37cba78bc
MD15 <36522826+MD15@users.noreply.github.com>
2020-04-10 12:11:09 +0700

    Create TransposeMatrix.c

---
+++ 03. Ekspresi dan Masukan/TransposeMatrix.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+	int a,b,c,d,e,f,g,h,i;
+	scanf("%d %d %d",&a, &b, &c);
+	scanf("%d %d %d",&d, &e, &f);
+	scanf("%d %d %d",&g, &h, &i);
+	
+	printf("%d %d %d\n",a,d,g);
+	printf("%d %d %d\n",b,e,h);
+	printf("%d %d %d\n",c,f,i);
+}
